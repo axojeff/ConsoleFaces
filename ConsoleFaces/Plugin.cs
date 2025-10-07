645792bb532d782b22114838fbc4cb43f6b55f7e
axojeff <jacksonpope027@gmail.com>
2025-10-06 18:19:47 -0600

    CREATED DA MOD :ShockedHog:

---
+++ ConsoleFaces/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace ConsoleFaces
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Init()
+        {
+            CosmeticsV2Spawner_Dirty.OnPostInstantiateAllPrefabs += (() =>
+            {
+                Debug.Log(consolefaces.Value);
+            });
+        }
+        private void Awake()
+        {
+            consolefaces =
+                ConsoleFaces.Bind("ConsoleFaces", "ConsoleFaces", ":3", "What the mod sends into the console");
+        }
+        private void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(Init);
+          
+        }
+        private ConfigFile ConsoleFaces = new ConfigFile(Path.Combine(Paths.ConfigPath, "axo.ConsoleFaces.cfg"), true);
+        private ConfigEntry<string> consolefaces;
+            
+    }
+
+}
---
+++ ConsoleFaces/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleFaces
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.axo.gorillatag.consolefaces";
+        public const string Name = "ConsoleFaces";
+        public const string Version = "1.0.0";
+    }
+}
