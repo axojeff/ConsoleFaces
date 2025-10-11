87370a96b7405c7559503dd0a64511c19da0d56a
Lapis <47952866+LapisGit@users.noreply.github.com>
2025-10-11 10:04:54 -0400

    remove unessacary events (you dont need those dude)

--- ConsoleFaces/Plugin.cs
+++ ConsoleFaces/Plugin.cs
@@ -10,22 +10,11 @@ namespace ConsoleFaces
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Init()
-        {
-            CosmeticsV2Spawner_Dirty.OnPostInstantiateAllPrefabs += (() =>
-            {
-                Debug.Log(consolefaces.Value);
-            });
-        }
         private void Awake()
         {
             consolefaces =
                 ConsoleFaces.Bind("ConsoleFaces", "ConsoleFaces", ":3", "What the mod sends into the console");
-        }
-        private void Start()
-        {
-            GorillaTagger.OnPlayerSpawned(Init);
-          
+            Debug.Log(consolefaces.Value);
         }
         private ConfigFile ConsoleFaces = new ConfigFile(Path.Combine(Paths.ConfigPath, "axo.ConsoleFaces.cfg"), true);
         private ConfigEntry<string> consolefaces;
